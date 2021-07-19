24ed153a14a005cd8611d9b47a6c35fb10c5bffd
Ujjwal Gupta <51078583+ujjwalgupt97@users.noreply.github.com>
2021-07-19 22:42:42 +0530

    Update custom_resnet.py

--- models/custom_resnet.py
+++ models/custom_resnet.py
@@ -5,57 +5,63 @@ import torch.nn.functional as F
 class CustomResnet(nn.Module):
     def __init__(self):
         super(CustomResnet, self).__init__()
-
+        
+        # Prep Layer
         self.preplayer = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3, 3), padding=1, bias=False), # 32 32  64
             nn.BatchNorm2d(64),
             nn.ReLU(),
         )
-
+        
+        # Layer 1
         self.x1 = nn.Sequential(
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
-            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), padding=1, bias=False), # 32 32 128
+            nn.MaxPool2d(2, 2),                                                                     # 32 16 128
             nn.BatchNorm2d(128),
             nn.ReLU(),
         )
-
+        # ResneT block
         self.res1 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1, bias=False), # 16 16 128
             nn.BatchNorm2d(128),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), padding=1, bias=False), # 16 16 128
             nn.BatchNorm2d(128),
             nn.ReLU(),
         )
-
+        
+        # Layer 2
         self.layer2 = nn.Sequential(
-            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1, bias=False),
-            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), padding=1, bias=False),  # 16 16 256
+            nn.MaxPool2d(2, 2),                                                                       # 16 8 256
             nn.BatchNorm2d(256),
             nn.ReLU(),
         )
-
+        
+        # Layer 3
         self.x3 = nn.Sequential(
-            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding=1, bias=False),
-            nn.MaxPool2d(2, 2),
+            nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3, 3), padding=1, bias=False), # 8 8 512
+            nn.MaxPool2d(2, 2),                                                                      # 8 4 512
             nn.BatchNorm2d(512),
             nn.ReLU(),
         )
-
+        # resnet bloack
         self.res3 = nn.Sequential(
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1, bias=False), # 4 4 512
             nn.BatchNorm2d(512),
             nn.ReLU(),
 
-            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1, bias=False),
+            nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3, 3), padding=1, bias=False),# 4 4 512
             nn.BatchNorm2d(512),
             nn.ReLU(),
         )
-
-        self.max4d = nn.MaxPool2d(4, 4)
-
-        self.fc = nn.Linear(in_features=512, out_features=10, bias=False)
+        
+        # max pool 4D
+        self.max4d = nn.MaxPool2d(4, 4)                                                             # 4 1 512
+        
+        # Fully connected
+        self.fc = nn.Linear(in_features=512, out_features=10, bias=False)                           # 1 1 10
 
     def forward(self, x):
         prep_layer = self.preplayer(x)
